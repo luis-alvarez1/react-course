848ac0b4406751b367ed93af994414953d1f661c
Luis Alvarez <lealvarezp@unac.edu.co>
2023-04-09 01:22:02 -0500

    complete auth

--- redux-react/src/App.js
+++ redux-react/src/App.js
@@ -2,13 +2,23 @@ import Counter from './components/Counter';
 
 import Header from './components/Header';
 import Auth from './components/Auth';
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const isAuth = useSelector((state) => state.auth.isAuth);
+
+  const MainContent = () => (
+    <>
+      <UserProfile />
+      <Counter />
+    </>
+  );
+
   return (
     <>
       <Header />
-      <Auth />
-      <Counter />
+      {isAuth ? <MainContent /> : <Auth />}
     </>
   );
 }
